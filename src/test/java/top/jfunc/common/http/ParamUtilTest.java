821d536a62e1c99c50927459eb93154e3e28967c
xxssyyyyssxx <xxssyyyyssxx@126.com>
2019-03-28 17:29:00 +0800

    代码优化

--- src/test/java/top/jfunc/common/http/ParamUtilTest.java
+++ src/test/java/top/jfunc/common/http/ParamUtilTest.java
@@ -7,33 +7,35 @@ import org.junit.Test;
  * @author xiongshiyan at 2019/3/28 , contact me with email yanshixiong@126.com or phone 15208384257
  */
 public class ParamUtilTest {
+    private String completedUrlHttp = "http://localhost:8080/ssss";
+    private String completedUrlHttps = "https://localhost:8080/ssss";
     @Test
     public void testIsHttpIsHttps(){
-        Assert.assertTrue(ParamUtil.isHttps("https://localhost:8080/ssss"));
-        Assert.assertTrue(ParamUtil.isHttp("http://localhost:8080/ssss"));
-        Assert.assertFalse(ParamUtil.isHttp("https://localhost:8080/ssss"));
-        Assert.assertFalse(ParamUtil.isHttps("http://localhost:8080/ssss"));
+        Assert.assertTrue(ParamUtil.isHttps(completedUrlHttps));
+        Assert.assertTrue(ParamUtil.isHttp(completedUrlHttp));
+        Assert.assertFalse(ParamUtil.isHttp(completedUrlHttps));
+        Assert.assertFalse(ParamUtil.isHttps(completedUrlHttp));
 
         Assert.assertFalse(ParamUtil.isHttps("/ssss"));
 
         Assert.assertFalse(ParamUtil.isCompletedUrl("/ssss"));
-        Assert.assertTrue(ParamUtil.isCompletedUrl("https://localhost:8080/ssss"));
-        Assert.assertTrue(ParamUtil.isCompletedUrl("http://localhost:8080/ssss"));
+        Assert.assertTrue(ParamUtil.isCompletedUrl(completedUrlHttps));
+        Assert.assertTrue(ParamUtil.isCompletedUrl(completedUrlHttp));
     }
 
     @Test
     public void testAddBaseUrlIfNecessary(){
-        Assert.assertEquals("https://localhost:8080/ssss" ,
-                ParamUtil.addBaseUrlIfNecessary(null , "https://localhost:8080/ssss"));
-        Assert.assertEquals("https://localhost:8080/ssss" ,
-                ParamUtil.addBaseUrlIfNecessary("https://localhost:8080" , "https://localhost:8080/ssss"));
-        Assert.assertEquals("https://localhost:8080/ssss" ,
+        Assert.assertEquals(completedUrlHttps ,
+                ParamUtil.addBaseUrlIfNecessary(null , completedUrlHttps));
+        Assert.assertEquals(completedUrlHttps ,
+                ParamUtil.addBaseUrlIfNecessary("https://localhost:8080" , completedUrlHttps));
+        Assert.assertEquals(completedUrlHttps ,
                 ParamUtil.addBaseUrlIfNecessary("https://localhost:8080/" , "/ssss"));
-        Assert.assertEquals("https://localhost:8080/ssss" ,
+        Assert.assertEquals(completedUrlHttps ,
                 ParamUtil.addBaseUrlIfNecessary("https://localhost:8080" , "ssss"));
-        Assert.assertEquals("https://localhost:8080/ssss" ,
+        Assert.assertEquals(completedUrlHttps ,
                 ParamUtil.addBaseUrlIfNecessary("https://localhost:8080/" , "ssss"));
-        Assert.assertEquals("https://localhost:8080/ssss" ,
+        Assert.assertEquals(completedUrlHttps ,
                 ParamUtil.addBaseUrlIfNecessary("https://localhost:8080" , "/ssss"));
     }
 }
