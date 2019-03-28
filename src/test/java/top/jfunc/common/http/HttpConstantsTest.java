915dc14dd7862c3193cbede297ddda40798de61f
xxssyyyyssxx <xxssyyyyssxx@126.com>
2019-03-28 18:52:11 +0800

    代码优化

--- src/test/java/top/jfunc/common/http/HttpConstantsTest.java
+++ src/test/java/top/jfunc/common/http/HttpConstantsTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 public class HttpConstantsTest {
     @Test
     public void testInit(){
-        Assert.assertEquals(15000 , HttpConstants.DEFAULT_CONNECT_TIMEOUT);
-        Assert.assertEquals(15000 , HttpConstants.DEFAULT_READ_TIMEOUT);
+        Assert.assertEquals(Integer.valueOf(15000) , HttpConstants.DEFAULT_CONNECT_TIMEOUT);
+        Assert.assertEquals(Integer.valueOf(15000) , HttpConstants.DEFAULT_READ_TIMEOUT);
         Assert.assertEquals("UTF-8" , HttpConstants.DEFAULT_CHARSET);
     }
 
@@ -16,14 +16,14 @@ public class HttpConstantsTest {
     @Ignore
     public void testSetProp(){
         System.setProperty("DEFAULT_CONNECT_TIMEOUT" , "20000");
-        Assert.assertEquals(20000 , HttpConstants.DEFAULT_CONNECT_TIMEOUT);
-        Assert.assertEquals(15000 , HttpConstants.DEFAULT_READ_TIMEOUT);
+        Assert.assertEquals(Integer.valueOf(20000) , HttpConstants.DEFAULT_CONNECT_TIMEOUT);
+        Assert.assertEquals(Integer.valueOf(15000) , HttpConstants.DEFAULT_READ_TIMEOUT);
     }
     @Test
     @Ignore
     public void testSetD(){
         //-D转入
-        Assert.assertEquals(15000 , HttpConstants.DEFAULT_CONNECT_TIMEOUT);
-        Assert.assertEquals(20000 , HttpConstants.DEFAULT_READ_TIMEOUT);
+        Assert.assertEquals(Integer.valueOf(15000) , HttpConstants.DEFAULT_CONNECT_TIMEOUT);
+        Assert.assertEquals(Integer.valueOf(20000) , HttpConstants.DEFAULT_READ_TIMEOUT);
     }
 }
