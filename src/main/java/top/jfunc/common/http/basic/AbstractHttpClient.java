bbc4a9648bd9ea433b20292753935e963685586a
xxssyyyyssxx <xxssyyyyssxx@126.com>
2019-07-09 20:43:55 +0800

    勿以善小而不为

--- src/main/java/top/jfunc/common/http/basic/AbstractHttpClient.java
+++ src/main/java/top/jfunc/common/http/basic/AbstractHttpClient.java
@@ -17,7 +17,7 @@ import java.util.Map;
  * 实现者只需要实现HttpTemplate接口、处理POST Body、文件上传Body即可
  * @see HttpTemplate
  * @see AbstractHttpClient#bodyContentCallback(String, String, String)
- * @see AbstractHttpClient#uploadContentCallback(MultiValueMap, FormFile[])
+ * @see AbstractHttpClient#uploadContentCallback(MultiValueMap, String, FormFile[])
  * @author xiongshiyan at 2019/5/9 , contact me with email yanshixiong@126.com or phone 15208384257
  */
 public abstract class AbstractHttpClient<CC> extends AbstractConfigurableHttp implements HttpClient, HttpTemplate<CC> {
@@ -36,16 +36,6 @@ public abstract class AbstractHttpClient<CC> extends AbstractConfigurableHttp im
                 connectTimeout, readTimeout , resultCharset,false, (s, b,r,h)-> IoUtil.read(b ,r));
     }
 
-    /**
-     * 处理请求体的回调
-     * @param body 请求体
-     * @param bodyCharset 编码
-     * @param contentType Content-Type
-     * @return ContentCallback<CC>
-     * @throws IOException IOException
-     */
-    abstract protected ContentCallback<CC> bodyContentCallback(String body , String bodyCharset , String contentType) throws IOException;
-
     @Override
     public byte[] getAsBytes(String url, MultiValueMap<String, String> headers, Integer connectTimeout, Integer readTimeout) throws IOException {
         return template(url, Method.GET,null,null, headers,
@@ -63,22 +53,33 @@ public abstract class AbstractHttpClient<CC> extends AbstractConfigurableHttp im
 
     @Override
     public String upload(String url, MultiValueMap<String,String> headers, Integer connectTimeout, Integer readTimeout, String resultCharset, FormFile... files) throws IOException{
-        return template(url, Method.POST, null, uploadContentCallback(null , files),
+        return template(url, Method.POST, null, uploadContentCallback(null , getDefaultBodyCharset() , files),
                 headers, connectTimeout, readTimeout , resultCharset,false, (s, b,r,h)-> IoUtil.read(b ,r));
     }
 
     @Override
     public String upload(String url, MultiValueMap<String, String> params, MultiValueMap<String, String> headers, Integer connectTimeout, Integer readTimeout, String resultCharset, FormFile... files) throws IOException {
-        return template(url, Method.POST, null, uploadContentCallback(params , files),
+        return template(url, Method.POST, null, uploadContentCallback(params , getDefaultBodyCharset() , files),
                 headers, connectTimeout, readTimeout , resultCharset,false, (s, b,r,h)-> IoUtil.read(b ,r));
     }
 
     /**
+     * 处理请求体的回调
+     * @param body 请求体
+     * @param bodyCharset 编码
+     * @param contentType Content-Type
+     * @return ContentCallback<CC>
+     * @throws IOException IOException
+     */
+    abstract protected ContentCallback<CC> bodyContentCallback(String body , String bodyCharset , String contentType) throws IOException;
+
+    /**
      * 处理文件上传
      * @param params 参数，k-v，可能为null
+     * @param paramCharset 参数编码
      * @param formFiles 文件信息
      * @return ContentCallback<CC>
      * @throws IOException IOException
      */
-    protected abstract ContentCallback<CC> uploadContentCallback(MultiValueMap<String, String> params , FormFile[] formFiles) throws IOException;
+    protected abstract ContentCallback<CC> uploadContentCallback(MultiValueMap<String, String> params , String paramCharset , FormFile[] formFiles) throws IOException;
 }
