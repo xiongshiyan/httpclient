8dc09dc3dfeb2b2f8d696692803dba390121a181
xxssyyyyssxx <xxssyyyyssxx@126.com>
2019-07-09 21:07:47 +0800

    实现基于抽象类的，但是也会存在重复代码。。。

--- src/main/java/top/jfunc/common/http/smart/ApacheSmartHttpClient.java
+++ src/main/java/top/jfunc/common/http/smart/ApacheSmartHttpClient.java
@@ -11,28 +11,25 @@ import org.apache.http.util.EntityUtils;
 import top.jfunc.common.http.Method;
 import top.jfunc.common.http.ParamUtil;
 import top.jfunc.common.http.base.ContentCallback;
+import top.jfunc.common.http.base.FormFile;
 import top.jfunc.common.http.base.ResultCallback;
-import top.jfunc.common.http.basic.ApacheHttpClient;
-import top.jfunc.common.http.request.DownloadRequest;
 import top.jfunc.common.http.request.HttpRequest;
-import top.jfunc.common.http.request.StringBodyRequest;
-import top.jfunc.common.http.request.UploadRequest;
 import top.jfunc.common.utils.IoUtil;
 import top.jfunc.common.utils.MultiValueMap;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.CookieHandler;
 import java.net.URI;
 
+import static top.jfunc.common.http.basic.ApacheUtil.*;
 /**
  * 使用Apache HttpClient 实现的Http请求类
  * @author 熊诗言2017/12/01
  */
-public class ApacheSmartHttpClient extends ApacheHttpClient implements SmartHttpClient, SmartInterceptorHttpTemplate<HttpEntityEnclosingRequest> {
+public class ApacheSmartHttpClient extends AbstractSmartHttpClient<HttpEntityEnclosingRequest> implements SmartHttpClient, SmartInterceptorHttpTemplate<HttpEntityEnclosingRequest> {
 
     @Override
     public <R> R doTemplate(HttpRequest httpRequest, Method method , ContentCallback<HttpEntityEnclosingRequest> contentCallback , ResultCallback<R> resultCallback) throws IOException {
@@ -127,51 +124,72 @@ public class ApacheSmartHttpClient extends ApacheHttpClient implements SmartHttp
             IoUtil.close(httpClient);
         }
     }
-
     @Override
-    public Response get(HttpRequest request) throws IOException {
-        return template(request , Method.GET , null , Response::with);
-    }
+    public  <R> R template(String url, Method method , String contentType, ContentCallback<HttpEntityEnclosingRequest> contentCallback, MultiValueMap<String, String> headers, Integer connectTimeout, Integer readTimeout, String resultCharset , boolean includeHeader , ResultCallback<R> resultCallback) throws IOException {
+        //1.获取完成的URL，创建请求
+        String completedUrl = addBaseUrlIfNecessary(url);
+        ///*URIBuilder builder = new URIBuilder(url);
+        //URI uri = builder.build();*/
+        HttpUriRequest httpUriRequest = createHttpUriRequest(completedUrl , method);
 
-    @Override
-    public Response post(StringBodyRequest request) throws IOException {
-        final String body = request.getBody();
-        final String bodyCharset = calculateBodyCharset(request.getBodyCharset() , request.getContentType());
-        return template(request, Method.POST ,
-                r -> setRequestBody(r, body, bodyCharset), Response::with);
-    }
 
-    @Override
-    public <R> R http(HttpRequest request, Method method, ResultCallback<R> resultCallback) throws IOException {
-        ContentCallback<HttpEntityEnclosingRequest> contentCallback = null;
-        if(method.hasContent() && request instanceof StringBodyRequest){
-            StringBodyRequest bodyRequest = (StringBodyRequest) request;
-            final String body = bodyRequest.getBody();
-            final String bodyCharset = calculateBodyCharset(bodyRequest.getBodyCharset() , bodyRequest.getContentType());
-            contentCallback = r -> setRequestBody(r, body, bodyCharset);
+        //2.设置请求参数
+        setRequestProperty((HttpRequestBase) httpUriRequest,
+                getConnectionTimeoutWithDefault(connectTimeout),
+                getReadTimeoutWithDefault(readTimeout));
+
+        //3.创建请求内容，如果有的话
+        if(httpUriRequest instanceof HttpEntityEnclosingRequest){
+            if(contentCallback != null){
+                contentCallback.doWriteWith((HttpEntityEnclosingRequest)httpUriRequest);
+            }
         }
-        return template(request, method , contentCallback, resultCallback);
+
+        //4.设置请求头
+        setRequestHeaders(httpUriRequest, contentType, mergeDefaultHeaders(headers) , null);
+
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
+        HttpEntity entity = null;
+        try {
+
+            //5.创建http客户端
+            ///CloseableHttpClient httpClient = HttpClients.createDefault();
+            httpClient = getCloseableHttpClient(completedUrl ,getHostnameVerifier() , getSSLContext());
+
+            //6.发送请求
+            response = httpClient.execute(httpUriRequest  , HttpClientContext.create());
+            int statusCode = response.getStatusLine().getStatusCode();
+            /*String resultString = EntityUtils.toString(response.getEntity(), resultCharset);*/
+
+            entity = response.getEntity();
+
+            InputStream inputStream = getStreamFrom(entity, false);
+
+            R convert = resultCallback.convert(statusCode , inputStream, getResultCharsetWithDefault(resultCharset),  parseHeaders(response , includeHeader));
+            IoUtil.close(inputStream);
+
+            return convert;
+        } catch (IOException e) {
+            throw e;
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }finally {
+            EntityUtils.consumeQuietly(entity);
+            IoUtil.close(response);
+            IoUtil.close(httpClient);
+        }
     }
 
     @Override
-    public byte[] getAsBytes(HttpRequest request) throws IOException {
-        return template(request , Method.GET , null , (s, b, r, h)-> IoUtil.stream2Bytes(b));
+    protected ContentCallback<HttpEntityEnclosingRequest> bodyContentCallback(String body, String bodyCharset, String contentType) throws IOException {
+        return (request -> setRequestBody(request , body , bodyCharset));
     }
 
     @Override
-    public File download(DownloadRequest request) throws IOException {
-        return template(request , Method.GET, null , (s, b, r, h)-> IoUtil.copy2File(b, request.getFile()));
+    protected ContentCallback<HttpEntityEnclosingRequest> uploadContentCallback(MultiValueMap<String, String> params, String paramCharset, FormFile[] formFiles) throws IOException {
+        return (request -> upload0(request, params , paramCharset , formFiles));
     }
-
-    @Override
-    public Response upload(UploadRequest request) throws IOException {
-        return template(request , Method.POST ,
-                r -> upload0(r, request.getFormParams(),
-                            calculateBodyCharset(request.getParamCharset() , request.getContentType()),
-                            request.getFormFiles())
-                , Response::with);
-    }
-
     @Override
     public String toString() {
         return "SmartHttpClient implemented by Apache's httpcomponents";
