5a0d93bfdd9c57915398e17a0e684ac9b5fb6abd
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-01-10 09:01:13 +0800

    CloseableHttpClient没必要用builder

--- src/main/java/top/jfunc/common/http/component/apache/DefaultApacheClientFactory.java
+++ src/main/java/top/jfunc/common/http/component/apache/DefaultApacheClientFactory.java
@@ -1,11 +1,12 @@
 package top.jfunc.common.http.component.apache;
 
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import top.jfunc.common.http.Method;
 import top.jfunc.common.http.ParamUtil;
 import top.jfunc.common.http.base.Config;
-import top.jfunc.common.http.request.HttpRequest;
 import top.jfunc.common.http.component.AbstractRequesterFactory;
+import top.jfunc.common.http.request.HttpRequest;
 import top.jfunc.common.http.util.ApacheUtil;
 
 import javax.net.ssl.HostnameVerifier;
@@ -15,9 +16,9 @@ import java.io.IOException;
 /**
  * @author xiongshiyan at 2020/1/6 , contact me with email yanshixiong@126.com or phone 15208384257
  */
-public class DefaultApacheClientBuilderFactory extends AbstractRequesterFactory<HttpClientBuilder> {
+public class DefaultApacheClientFactory extends AbstractRequesterFactory<CloseableHttpClient> {
     @Override
-    public HttpClientBuilder doCreate(HttpRequest httpRequest, Method method, String completedUrl) throws IOException {
+    public CloseableHttpClient doCreate(HttpRequest httpRequest, Method method, String completedUrl) throws IOException {
         Config config = httpRequest.getConfig();
         ////////////////////////////////////ssl处理///////////////////////////////////
         HostnameVerifier hostnameVerifier = null;
@@ -29,6 +30,7 @@ public class DefaultApacheClientBuilderFactory extends AbstractRequesterFactory<
         }
         ////////////////////////////////////ssl处理///////////////////////////////////
 
-        return ApacheUtil.getCloseableHttpClientBuilder(completedUrl, hostnameVerifier, sslContext, httpRequest.followRedirects());
+        HttpClientBuilder clientBuilder = ApacheUtil.getCloseableHttpClientBuilder(completedUrl, hostnameVerifier, sslContext, httpRequest.followRedirects());
+        return clientBuilder.build();
     }
 }
