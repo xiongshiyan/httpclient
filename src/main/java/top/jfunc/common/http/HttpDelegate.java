8a841c77ef7130933405032476e2cf8186ac23af
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-01-29 20:16:12 +0800

    HttpDelegate优化

--- src/main/java/top/jfunc/common/http/HttpDelegate.java
+++ src/main/java/top/jfunc/common/http/HttpDelegate.java
@@ -1,6 +1,5 @@
 package top.jfunc.common.http;
 
-import top.jfunc.common.http.smart.NativeSmartHttpClient;
 import top.jfunc.common.http.smart.SmartHttpClient;
 import top.jfunc.common.utils.ClassUtil;
 
@@ -24,23 +23,23 @@ public class HttpDelegate {
         SmartHttpClient delegateToUse = null;
         // okhttp3.OkHttpClient ?
         if (ClassUtil.isPresent(HttpDelegate.class.getClassLoader() ,
-                "okhttp3.OkHttpClient" , "okio.Okio")) {
+                "top.jfunc.common.http.smart.OkHttp3SmartHttpClient")) {
             delegateToUse = new top.jfunc.common.http.smart.OkHttp3SmartHttpClient();
         }
         // org.apache.http.impl.client.CloseableHttpClient ?
         else if (ClassUtil.isPresent(HttpDelegate.class.getClassLoader() ,
-                "org.apache.http.impl.client.CloseableHttpClient","org.apache.http.impl.client.HttpClientBuilder")) {
+                "top.jfunc.common.http.smart.ApacheSmartHttpClient")) {
             delegateToUse = new top.jfunc.common.http.smart.ApacheSmartHttpClient();
         }
         // jodd.http.HttpRequest ?
         else if (ClassUtil.isPresent(HttpDelegate.class.getClassLoader() ,
-                "jodd.http.HttpRequest","jodd.http.HttpResponse")) {
+                "top.jfunc.common.http.smart.JoddSmartHttpClient")) {
             delegateToUse = new top.jfunc.common.http.smart.JoddSmartHttpClient();
         }
         // java.net.URLConnection
-        else {//if (ClassUtil.isPresent(HttpDelegate.class.getClassLoader() ,
-              //  "java.net.URLConnection")) {
-            delegateToUse = new NativeSmartHttpClient();
+        else if (ClassUtil.isPresent(HttpDelegate.class.getClassLoader() ,
+              "top.jfunc.common.http.smart.NativeSmartHttpClient")) {
+            delegateToUse = new top.jfunc.common.http.smart.NativeSmartHttpClient();
         }
         return delegateToUse;
     }
