5cef9ea23d54518613fd347c8f298f4d131ec16f
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-02-17 17:35:02 +0800

    仅支持浅克隆

--- src/test/java/top/jfunc/common/http/CloneTest.java
+++ src/test/java/top/jfunc/common/http/CloneTest.java
@@ -1,7 +1,9 @@
 package top.jfunc.common.http;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import top.jfunc.common.http.base.FormFile;
+import top.jfunc.common.http.holderrequest.impl.HolderUpLoadRequest;
 import top.jfunc.common.http.request.basic.UpLoadRequest;
 
 import java.io.File;
@@ -9,6 +11,7 @@ import java.io.File;
 /**
  * @author xiongshiyan at 2020/2/17 , contact me with email yanshixiong@126.com or phone 15208384257
  */
+@Ignore
 public class CloneTest {
     @Test
     public void testClone() throws Exception{
@@ -16,9 +19,30 @@ public class CloneTest {
         upLoadRequest.addFormParam("k1" , "v1");
         upLoadRequest.setParamCharset("dsd");
         upLoadRequest.addFormFile(new FormFile(new File("C:\\Users\\xiongshiyan\\Desktop\\加班.txt") , "dsad" ,"sda"));
+        upLoadRequest.followRedirects(true);
+
+
         UpLoadRequest clone = upLoadRequest.clone();
 
-        clone.followRedirects(true);
+        clone.followRedirects(false);
+        clone.setIncludeHeaders(true);
+        clone.setIgnoreResponseBody(true);
+        clone.addFormParam("k2" , "v2");
+        clone.addQueryParam("dd" , "sdsad");
+        System.out.println(clone);
+    }
+    @Test
+    public void testClone2() throws Exception{
+        HolderUpLoadRequest upLoadRequest = HolderUpLoadRequest.of("sss");
+        upLoadRequest.addFormParam("k1" , "v1");
+        upLoadRequest.setParamCharset("dsd");
+        upLoadRequest.addFormFile(new FormFile(new File("C:\\Users\\xiongshiyan\\Desktop\\加班.txt") , "dsad" ,"sda"));
+        upLoadRequest.followRedirects(true);
+
+
+        HolderUpLoadRequest clone = upLoadRequest.clone();
+
+        clone.followRedirects(false);
         clone.setIncludeHeaders(true);
         clone.setIgnoreResponseBody(true);
         clone.addFormParam("k2" , "v2");
