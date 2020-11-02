6f244124f37f3a2125cb2a4f30be4cffb9a8878e
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-11-02 12:03:28 +0800

    Cookie的实现参考jodd，更完美

--- src/test/java/top/jfunc/common/http/CookieTest.java
+++ src/test/java/top/jfunc/common/http/CookieTest.java
@@ -1,16 +1,18 @@
 package top.jfunc.common.http;
 
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.junit.MockServerRule;
 import top.jfunc.common.http.base.Config;
-import top.jfunc.common.http.cookie.CookieJar;
-import top.jfunc.common.http.cookie.JdkCookieJar;
+import top.jfunc.common.http.cookie.CookieStore;
+import top.jfunc.common.http.cookie.InMemoryCookieStore;
 import top.jfunc.common.http.holderrequest.impl.HolderGetRequest;
 import top.jfunc.common.http.smart.*;
 import top.jfunc.common.utils.MultiValueMap;
 
-import java.net.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -26,88 +28,34 @@ public class CookieTest {
     @Rule
     public MockServerRule server = new MockServerRule(this, 50000);
 
-    @Test
-    @Ignore
-    public void testAddCookie() throws Exception{
-        // 创建一个 CookieManager对象
-        CookieManager manager = new CookieManager();
-        // 接受所有的Cookie
-        manager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-        // 保存这个定制的CookieManager
-        CookieHandler.setDefault(manager);
-        //CookieStore cookieJar = manager.getCookieStore();
-        // 用List获取cookie，因为cookie中可能包含多个信息
-        //List<HttpCookie> cookies = cookieJar.getCookies();
-
-
-        //请求的header：从cookiejar中获取，这些写入header ： requestheaders不为空即可
-        //Map<String, List<String>> stringListMap = manager.get(new URL("").toURI(), null);
-        /**其他的操作省略*/
-        //从返回的header中获取cookie保存 responseheader就是返回的header
-        //manager.put(new URL("").toURI() , null);
-
-        //写入request
-
-        /*cookies.forEach(httpCookie -> {
-            String cookie = httpCookie.toString();
-            System.out.println("Cookie:" + cookie);
-        });
-*/
-        // 这里我们就获取到了cookie，将其返回。
-
-        //return cookies;
-    }
-
-
-    /**
-     * 一个全局的CookieJar
-     */
-    private CookieJar cookieJar;
-
-    @Before
-    public void init(){
-        // 创建一个 CookieManager对象
-        CookieManager manager = new CookieManager();
-        // 接受所有的Cookie
-        manager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-        // 保存这个定制的CookieManager
-        //可调可不调
-        // CookieHandler.setDefault(manager);
-
-        this.cookieJar = new JdkCookieJar(manager , true);
-    }
-
-    @After
-    public void destroy(){
-        cookieJar = null;
-    }
+    private CookieStore cookieStore = new InMemoryCookieStore();
 
     @Test
     public void testCookieOkHttp3()throws Exception{
-        //只要设置了CookieHandler就支持Cookie
+        //只要设置了CookieStore就支持Cookie
         SmartHttpClient smartHttpClient = new OkHttp3SmartHttpClient();
-        smartHttpClient.setConfig(Config.defaultConfig().setCookieJar(cookieJar));
+        smartHttpClient.setConfig(Config.defaultConfig().setCookieStore(cookieStore));
         testCookie(smartHttpClient);
     }
     @Test
     public void testCookieApacheHttp()throws Exception{
-        //只要设置了CookieHandler就支持Cookie
+        //只要设置了CookieStore就支持Cookie
         SmartHttpClient smartHttpClient = new ApacheSmartHttpClient();
-        smartHttpClient.setConfig(Config.defaultConfig().setCookieJar(cookieJar));
+        smartHttpClient.setConfig(Config.defaultConfig().setCookieStore(cookieStore));
         testCookie(smartHttpClient);
     }
     @Test
     public void testCookieNativeHttp()throws Exception{
-        //只要设置了CookieHandler就支持Cookie
+        //只要设置了CookieStore就支持Cookie
         SmartHttpClient smartHttpClient = new NativeSmartHttpClient();
-        smartHttpClient.setConfig(Config.defaultConfig().setCookieJar(cookieJar));
+        smartHttpClient.setConfig(Config.defaultConfig().setCookieStore(cookieStore));
         testCookie(smartHttpClient);
     }
     @Test
     public void testCookieJoddHttp() throws Exception{
-        //只要设置了CookieHandler就支持Cookie
+        //只要设置了CookieStore就支持Cookie
         SmartHttpClient smartHttpClient = new JoddSmartHttpClient();
-        smartHttpClient.setConfig(Config.defaultConfig().setCookieJar(cookieJar));
+        smartHttpClient.setConfig(Config.defaultConfig().setCookieStore(cookieStore));
         testCookie(smartHttpClient);
     }
 
