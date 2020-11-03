786edd7d1a11de24a2d5b647921d525a822b0286
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-11-03 18:35:33 +0800

    方法优化：使之具备可测试性

--- src/test/java/top/jfunc/common/http/CookieTest.java
+++ src/test/java/top/jfunc/common/http/CookieTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.junit.MockServerRule;
 import top.jfunc.common.http.base.Config;
+import top.jfunc.common.http.cookie.Cookie;
 import top.jfunc.common.http.cookie.CookieStore;
 import top.jfunc.common.http.cookie.InMemoryCookieStore;
 import top.jfunc.common.http.holderrequest.impl.HolderGetRequest;
@@ -14,6 +15,7 @@ import top.jfunc.common.http.smart.*;
 import top.jfunc.common.utils.MultiValueMap;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -102,5 +104,31 @@ public class CookieTest {
         Response response1 = smartHttpClient.get(HolderGetRequest.of("http://localhost:50000/hello/setCookie"));
         Assert.assertEquals("body2", response1.getBodyAsString());
         Assert.assertEquals(200 , response1.getStatusCode());
+    }
+
+
+    @Test
+    public void testParse(){
+        String cookieValue = "oschina_new_user=false; path=/; expires=Sat, 03 Nov 2040 10:05:31 -0000";
+        Cookie cookie = new Cookie(cookieValue);
+        Assert.assertEquals("oschina_new_user=false; Expires=Sat, 03 Nov 2040 10:05:31 -0000; Path=/", cookie.toString());
+
+        //test request cookies string build
+        Cookie cookie1 = new Cookie("user_locale", "zh-CN");
+        Cookie cookie2 = new Cookie("gitee-session-n", "Mk9HQnhFVTRIL1Z");
+        String cookieString = Cookie.buildCookieString(Arrays.asList(cookie1, cookie2));
+        Assert.assertEquals("user_locale=zh-CN; gitee-session-n=Mk9HQnhFVTRIL1Z", cookieString);
+
+        //test response cookies parse
+        List<String> list = Arrays.asList("oschina_new_user=false; path=/; expires=Sat, 03 Nov 2040 10:05:31 -0000"
+                , "gitee-session-n=Mk9HQnhFVTRIL1Z; domain=.gitee.com; path=/; HttpOnly");
+        List<Cookie> cookies = Cookie.parseCookies(list);
+        Assert.assertEquals(2, cookies.size());
+        Assert.assertEquals("oschina_new_user",cookies.get(0).getName());
+        Assert.assertEquals("false",cookies.get(0).getValue());
+        Assert.assertEquals("gitee-session-n",cookies.get(1).getName());
+        Assert.assertEquals("Mk9HQnhFVTRIL1Z",cookies.get(1).getValue());
+        Assert.assertEquals(".gitee.com",cookies.get(1).getDomain());
+        Assert.assertEquals(true,cookies.get(1).isHttpOnly());
     }
 }
